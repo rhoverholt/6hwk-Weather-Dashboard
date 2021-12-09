7ab6862a2572df692906a5758da711aba0d54235
rhoverholt <rhoverholt@gmail.com>
2021-12-09 11:21:58 -0500

    updated comments in all files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,31 @@
 var cityInputEl = document.getElementById("city-input");
-const appID = "wthr-"
+const appID = "wthr-"; // used for local storage...I like to have app unique keys.
 
-// this populates any already visited cities -- to a max of 10.
+// this populates the list of buttons for already queried cities
 createCityHistory(cityInputEl);
 
-// This is the entire Form
+// This listens for clicks anywhere in the form, buttons, etc.
 cityInputEl.addEventListener("click", processClick);
 
+// Takes in the form element where the buttons need to be
+// and creates buttons for everything in local Storage.
+// In the future, I may want to limit this to 10 buttons or such.
 function createCityHistory(element) {
 
+	// note, the Array.from is needed, otherwise the result changes over time!
 	let historyList = Array.from(element.getElementsByClassName("city-from-history"));
 
+	// This is used later to know if a button has already been made or not.
 	let historyIdList = [];
-
 	for (a in historyList) {
 		historyIdList.push(historyList[a].id);
 	}
 
 	let keys = Object.keys(localStorage),
 		i = 0,
-		prevEl = document.getElementById("border-line");
+		prevEl = document.getElementById("border-line"); // used to place the new button on top of the button-list.
 
-	// read localStorage into lStorage
+	// read localStorage into lStorage - there was some funny sorting going on, so I enforce a sort by timestamp.
 	let lStorage = [];
 	while (i < keys.length) {
 		if (keys[i].substring(0,5) === appID) {
@@ -35,19 +39,16 @@ function createCityHistory(element) {
 	// sort local storage to show the earliest first
 	lStorage.sort(dynamicSort("timestamp"));
 
-
-	i = 0;
+	i = 0; // reset the index to 0 to loop through lStorage
 	while (i < lStorage.length) {
 
 		// Change, for example, "wthr-Altanta, Georgia, US" to "Atlanta-Georgia-US"
 		let btnId = lStorage[i].key.substring(5).replace(/\s/g,"").replace(/,/g,"-");
 				
-		// if the button already exists, ignore it for now //delete that and add it back here
+		// if the button already exists, ignore it for now 
 		if (!historyIdList.includes(btnId)) {
-		// 	deleteEl = document.getElementById(btnId);
-		// 	deleteEl.remove();
-		// }
-				
+			
+			// create a new button for this city
 			let newButton = document.createElement("button");
 			newButton.type = "button";
 			newButton.id = btnId;
@@ -56,15 +57,16 @@ function createCityHistory(element) {
 			newButton.dataset.lat = lStorage[i].cLat;
 			newButton.dataset.lon = lStorage[i].cLon;
 
-			prevEl.parentNode.insertBefore(newButton,prevEl.nextSibling);
+			prevEl.parentNode.insertBefore(newButton,prevEl.nextSibling); // place the new button on top, just after the border line.
 		}
 		i++;
 	}
 	
+	// I definitely just looked this one up...but it can be passed to sort() to define the sort order.
 	function dynamicSort(property) {
 		var sortOrder = 1;
 	
-		if(property[0] === "-") {
+		if(property[0] === "-") { // in case I later need to sort in reverse order
 			sortOrder = -1;
 			property = property.substr(1);
 		}
@@ -80,6 +82,7 @@ function createCityHistory(element) {
 
 }
 
+// This is the main listening function to process the user's input
 function processClick(event) {
 
 	// ignore irrelevant clicks
@@ -90,6 +93,7 @@ function processClick(event) {
 	// When the search button was pressed:
 	// - Prevent screen refresh/etc
 	// - Get the user entered city-name
+	// - leave the entered name in place for now as it might include a typo or such
 	if (event.target.id === "form-search") {
 		event.preventDefault();
 		let cityName = document.getElementById("city-name").value;
@@ -109,12 +113,15 @@ function processClick(event) {
 	let cLon = targetEl.dataset.lon;
 	let searchName = targetEl.textContent;
 
+	// this populates the weather fields on the screen through a call to the weather API.
 	getWeatherFromLatLon(searchName, cLat, cLon, weatherOneCall_CallBack, errorOneCall_CallBack);
 }
 
+// This gets called when valid weather data is returned from the API
+// This paints the weather data on the screen for the user.
 function weatherOneCall_CallBack(data, cityName) {
+	
 	// set current info
-
 	cityNameEl = document.getElementById("city-info-hdr");
 	cityTempEl = document.getElementById("city-temp");
 	cityWindEl = document.getElementById("city-wind");
@@ -129,7 +136,8 @@ function weatherOneCall_CallBack(data, cityName) {
 	cityWindEl.textContent = data.current.wind_speed + " MPH";
 	cityHumidityEl.textContent = data.current.humidity + "%";
 	cityUVIndexEl.textContent = data.current.uvi;
-	
+
+	// assign UV indexes -- note, most locations seem to be in the green zone!
 	if (cityUVIndexEl.textContent < 3) {
 		setUVclass(cityUVIndexEl, "bg-green");
 	} else if (cityUVIndexEl.textContent < 6) {
@@ -142,6 +150,7 @@ function weatherOneCall_CallBack(data, cityName) {
 		setUVclass(cityUVIndexEl, "bg-purple");
 	}
 
+	// this is where the weather icons are stored -- access them here rather than download into my assets for now.
 	var iconSrc = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon.substring(0,2) + "d@2x.png";
 	cityIconEl.src = iconSrc;
 	cityIconEl.alt = data.current.weather[0].main;
@@ -169,14 +178,16 @@ function weatherOneCall_CallBack(data, cityName) {
 	function setUVclass(element, className) {
 		isSet = false;
 		for (let cssClass of element.classList) {
-			if (cssClass.substring(0,3) === "bg-") {
+			if (cssClass.substring(0,3) === "bg-") { // these are just the UV classes...maybe I could have named them UV- instead!
 				if (cssClass === className) {
 					isSet = true;
 				} else {
+					// remove any UV classes that are not the one given.
 					element.classList.remove(cssClass);
 				}
 			}
 		}
+		// if our given class was not already included, add it.
 		if (!isSet) {element.classList.add(className)}
 	}
 }
@@ -187,21 +198,23 @@ function verifyCityName(cName) {
 		return;
 	}
 
+	// this call will validate the cName and make the appropriate weather calls to populate the screen
 	getWeatherFromCityName(cName, cityName_Callback, weatherOneCall_CallBack, errorOneCall_CallBack);
-	
 }
 
+// when a valid, new city has been searched on, make a button and store the info so user can just clikc and we can use lat/lon in the future
 function cityName_Callback(data) {
 
 	searchName = `${appID}${data[0].name}, ${((data[0].state != undefined) ? data[0].state + ',' : "")} ${data[0].country}`;
 
 	if (localStorage.getItem(searchName)) {
-		// errorMsg(`${searchName} is already in local storage`);
+		// if it's already in local storage, then it already has a button and we can simply remove the input field and exit.
 		inputTextEl = document.getElementById("city-name");
 		inputTextEl.value = "";
 		return;
 	}
 
+	// the rest simply creates the local storage and the button.
 	let cData = {
 		cName : data[0].name,
 		cState : data[0].state,
--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,23 +1,26 @@
+// This is my account specific key to access the weather data.  It should really be hidden somewhere, but is publicly accessible via this file,
 const apiKey = "&appid=2d0af2f2d383f295249a72ce853cc36b";
 
+// This is the primary URL that all the weather API's start from.
 const weatherUrlPrefix = "https://api.openweathermap.org/";
 
 //oneCall requires lat/lon to query
-//?lat=nnn&lon=nnn
 const oneCallUrl = weatherUrlPrefix + "data/2.5/onecall";
 const oneCallParams = "units=imperial&exclude=minutely,hourly" + apiKey;
 
+// geocode can translate a city name into a lat/lon so that oneCall can be used
 const geocodeUrl = weatherUrlPrefix + "geo/1.0/direct";
-const geocodeParams = "limit=5" + apiKey;
+const geocodeParams = "limit=5" + apiKey; // I could later implement logic to allow the user to select which city they desired.
 
-
+// This is an exported function that allows callers to retrieve weather given the noted inputs.
+// Note, the last two inputs are functions so the calling app can perform operations on these asynchronous results
 function getWeatherFromLatLon(cName, lat, lon, callbackFun, errorCallback) {
     requestUrl = oneCallUrl + `?lat=${lat}&lon=${lon}&${oneCallParams}`;
 
     fetch(requestUrl)
 	.then (function (response) {
 		if (response.status != 200) {
-			errorCallback("Weather currently unavailable.  Response Code: " + response.status);
+			errorCallback("Weather currently unavailable.  OneCall Response Code: " + response.status);
 			return;
 		}
 		return (response.json());
@@ -28,6 +31,8 @@ function getWeatherFromLatLon(cName, lat, lon, callbackFun, errorCallback) {
 	});
 }
 
+// This function first gets the lat/lon for a given cName, then calls the above function to return the actual weather data
+// Note, this is handled asynchronously, so must be supplied callback functions for the calling app to be able to process the data.
 function getWeatherFromCityName(cName, geoCallbackFun, callbackFun, errorCallback) {
 
 	// fetch the geocode info
@@ -58,12 +63,6 @@ function getWeatherFromCityName(cName, geoCallbackFun, callbackFun, errorCallbac
 		let lon = data[0].lon;
 		let cityName = `${data[0].name}, ${(data[0].state ? data[0].state + ',' : "")} ${data[0].country}`;
 
-		return getWeatherFromLatLon(cityName, lat, lon, callbackFun, errorCallback);
+		getWeatherFromLatLon(cityName, lat, lon, callbackFun, errorCallback);
 	}
 }
-
-// requestUrl = forecastUrl + cityTest + apiKey;
-// getWeather(requestUrl);
-
-// getting a city ID from a name, st, cntry:
-// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
